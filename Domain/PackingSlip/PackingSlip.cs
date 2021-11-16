330e09c68e08341e5eadc56418eb4be9b79a624a
ESipalis <eimantas98@gmail.com>
2021-11-16 03:45:17 +0100

    Domain: implement equality members for PackingSlip because it has an `ICollection<...>` property which needs to use `SequenceEqual` method (necessary for tests to pass).

--- Domain/PackingSlip/PackingSlip.cs
+++ Domain/PackingSlip/PackingSlip.cs
@@ -1,3 +1,20 @@
 ﻿namespace Domain.PackingSlip;
 
-public record PackingSlip(long OrderId, ICollection<PackingSlipItem> Items);
+public record PackingSlip(long OrderId, ICollection<PackingSlipItem> Items)
+{
+    // Equality members implemented because there is an ICollection property which needs to use SequenceEqual.
+    public virtual bool Equals(PackingSlip? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return OrderId == other.OrderId && Items.SequenceEqual(other.Items); // Sequence equal is necessary.
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (OrderId.GetHashCode() * 397) ^ Items.GetHashCode();
+        }
+    }
+}
