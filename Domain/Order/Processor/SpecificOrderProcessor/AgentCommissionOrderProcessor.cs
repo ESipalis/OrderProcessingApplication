ee25f15353dd5d21d01105912dc3172b1106983a
ESipalis <eimantas98@gmail.com>
2021-11-16 04:32:36 +0100

    Domain: AgentCommissionOrderProcessor implementation.

--- Domain/Order/Processor/SpecificOrderProcessor/AgentCommissionOrderProcessor.cs
+++ Domain/Order/Processor/SpecificOrderProcessor/AgentCommissionOrderProcessor.cs
@@ -1,23 +1,27 @@
-﻿using MediatR;
+﻿using Domain.Commands;
+using Domain.Order.SpecificOrderData;
+using MediatR;
 
 namespace Domain.Order.Processor.SpecificOrderProcessor;
 
 public class AgentCommissionOrderProcessor : ISpecificOrderProcessor
 {
+    private readonly IMediator _mediator;
     internal const decimal AgentCommissionValue = 7.11m;
     
     public AgentCommissionOrderProcessor(IMediator mediator)
     {
-        throw new InvalidOperationException();
+        _mediator = mediator;
     }
 
     public bool CheckOrderSuitabilityForProcessing(Order order)
     {
-        throw new InvalidOperationException();
+        return order.Data.TryPickT0(out PhysicalProductOrderData _, out _) || order.Data.TryPickT1(out BookOrderData _, out _);
     }
 
     public async Task ProcessOrder(Order order)
     {
-        throw new InvalidOperationException();
+        var addAgentCommissionCommand = new AddAgentCommissionCommand(order.AgentName, AgentCommissionValue);
+        await _mediator.Send(addAgentCommissionCommand);
     }
 }
