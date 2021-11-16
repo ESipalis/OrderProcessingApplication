e9701fa4dca746fc5249d8ffbde95e6f4753c767
ESipalis <eimantas98@gmail.com>
2021-11-16 05:38:50 +0100

    Infrastructure: implemented `DependencyInjection.AddInfrastructure`.

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+using Domain.Order.Processor;
+using Domain.Order.Processor.SpecificOrderProcessor;
+using Infrastructure.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<OrderProcessor>();
+        services.AddSingleton<ISpecificOrderProcessor, PhysicalProductOrderProcessor>();
+        services.AddSingleton<ISpecificOrderProcessor, BookOrderProcessor>();
+        services.AddSingleton<ISpecificOrderProcessor, AgentCommissionOrderProcessor>();
+        
+        services.AddSingleton<IPackingSlipService, DefaultPackingSlipService>();
+        services.AddSingleton<IAgentCommissionService, DefaultAgentCommissionService>();
+        services.AddMediatR(typeof(IPackingSlipService)); // Adds all handlers from Application layer.
+        return services;
+    }
+}
