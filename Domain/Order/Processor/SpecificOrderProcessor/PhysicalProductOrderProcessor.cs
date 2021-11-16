e4bf332612d48293a23e5ef56adb1304cca359f5
ESipalis <eimantas98@gmail.com>
2021-11-16 03:08:15 +0100

    Domain: PhysicalProductOrderProcessor skeleton.

---
+++ Domain/Order/Processor/SpecificOrderProcessor/PhysicalProductOrderProcessor.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Domain.Order.Processor.SpecificOrderProcessor;
+
+public class PhysicalProductOrderProcessor : ISpecificOrderProcessor
+{
+
+    public PhysicalProductOrderProcessor(IMediator mediator)
+    {
+        throw new InvalidOperationException();
+    }
+
+    public bool CheckOrderSuitabilityForProcessing(Order order)
+    {
+        throw new InvalidOperationException();
+    }
+
+    public async Task ProcessOrder(Order order)
+    {
+        throw new InvalidOperationException();
+    }
+}
