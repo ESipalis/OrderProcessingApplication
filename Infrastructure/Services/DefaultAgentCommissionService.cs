f285ed310d1c416de22c54b210f7271d2f73d179
ESipalis <eimantas98@gmail.com>
2021-11-16 05:00:02 +0100

    Infrastructure: default AgentCommissionService and PackingSlipService.

---
+++ Infrastructure/Services/DefaultAgentCommissionService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Services;
+
+public class DefaultAgentCommissionService : IAgentCommissionService
+{
+    private readonly ILogger<DefaultAgentCommissionService> _logger;
+
+    public DefaultAgentCommissionService(ILogger<DefaultAgentCommissionService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task AddCommissionForAgent(string agentName, decimal amount)
+    {
+        _logger.LogTrace("Adding {CommissionAmount} commission to agent '{AgentName}'", amount, agentName);
+        return Task.CompletedTask;
+    }
+}
---
+++ Infrastructure/Services/DefaultPackingSlipService.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services;
+using Domain.PackingSlip;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Services;
+
+public class DefaultPackingSlipService : IPackingSlipService
+{
+    private readonly ILogger<DefaultPackingSlipService> _logger;
+
+    public DefaultPackingSlipService(ILogger<DefaultPackingSlipService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task SendPackingSlip(PackingSlip packingSlip, string address)
+    {
+        _logger.LogTrace("Sending packing slip to {PackingSlipDestinationAddress}", address);
+        return Task.CompletedTask;
+    }
+}
