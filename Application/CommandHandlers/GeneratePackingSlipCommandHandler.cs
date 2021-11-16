aff4265d5287e2662d9b797e8faed16dadda4c33
ESipalis <eimantas98@gmail.com>
2021-11-16 04:42:17 +0100

    Application: `GeneratePackingSlipCommandHandler`.

---
+++ Application/CommandHandlers/GeneratePackingSlipCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services;
+using Domain.Commands;
+using MediatR;
+
+namespace Application.CommandHandlers;
+
+public class GeneratePackingSlipCommandHandler : IRequestHandler<GeneratePackingSlipCommand>
+{
+    private readonly IPackingSlipService _packingSlipService;
+
+    public GeneratePackingSlipCommandHandler(IPackingSlipService packingSlipService)
+    {
+        _packingSlipService = packingSlipService;
+    }
+
+    public async Task<Unit> Handle(GeneratePackingSlipCommand request, CancellationToken cancellationToken)
+    {
+        await _packingSlipService.SendPackingSlip(request.PackingSlip, request.Recipient.Address);
+        return Unit.Value;
+    }
+}
