9603c0f77430e2f325fc8aad0cc6e34d204b766e
ESipalis <eimantas98@gmail.com>
2021-11-16 03:25:14 +0100

    Domain.Tests.Unit: PhysicalProductOrderProcessor tests.

---
+++ Domain.Tests.Unit/Order/Processor/SpecificOrderProcessor/PhysicalProductOrderProcessorTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Commands;
+using Domain.Order.Processor.SpecificOrderProcessor;
+using Domain.Order.SpecificOrderData;
+using Domain.Order.SpecificOrderData.Extensions;
+using Domain.PackingSlip;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using Xunit;
+
+namespace Domain.Tests.Unit.Order.Processor.SpecificOrderProcessor;
+
+public class PhysicalProductOrderProcessorTests
+{
+    private readonly PhysicalProductOrderProcessor _sut;
+    private readonly Mock<IMediator> _mediatorMock;
+
+
+    public PhysicalProductOrderProcessorTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        _sut = new PhysicalProductOrderProcessor(_mediatorMock.Object);
+    }
+
+    [Fact]
+    public void CheckOrderSuitabilityForProcessing_ShouldReturnTrue_WhenItIsPhysicalProductOrder()
+    {
+        // Arrange
+        var order = new Domain.Order.Order(1, DateTimeOffset.Now, new PhysicalProductOrderData("123", 1));
+
+        // Act
+        bool result = _sut.CheckOrderSuitabilityForProcessing(order);
+        
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void CheckOrderSuitabilityForProcessing_ShouldReturnFalse_WhenItIsNOTPhysicalProductOrder()
+    {
+        // Arrange
+        var order = new Domain.Order.Order(1, DateTimeOffset.Now, new BookOrderData("bookName", "authorName", 1234));
+
+        // Act
+        bool result = _sut.CheckOrderSuitabilityForProcessing(order);
+        
+        // Assert
+        result.Should().BeFalse();
+    }
+    
+    [Fact]
+    public async Task ProcessOrder_ShouldSendGeneratePackingSlipCommand_WhenItIsPhysicalProductOrder()
+    {
+        // Arrange
+        var physicalProductOrderData = new PhysicalProductOrderData("123", 1);
+        var order = new Domain.Order.Order(1, DateTimeOffset.Now, physicalProductOrderData);
+
+        // Act
+        await _sut.ProcessOrder(order);
+        
+        // Assert
+        PackingSlip.PackingSlip packingSlip = physicalProductOrderData.GetPackingSlip(order);
+        var generatePackingSlipCommand = new GeneratePackingSlipCommand(packingSlip, new PackingSlipRecipient("WAREHOUSE", "Warehouse somewhere"));
+        _mediatorMock.Verify(x => x.Send(generatePackingSlipCommand, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ Domain/Order/SpecificOrderData/Extensions/PhysicalProductOrderDataExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Domain.PackingSlip;
+
+namespace Domain.Order.SpecificOrderData.Extensions;
+
+public static class PhysicalProductOrderDataExtensions
+{
+    public static PackingSlip.PackingSlip GetPackingSlip(this PhysicalProductOrderData physicalProductOrderData, Order order)
+    {
+        var packingSlipItems = new List<PackingSlipItem> { new(physicalProductOrderData.Sku, physicalProductOrderData.Quantity) };
+        return new PackingSlip.PackingSlip(order.Id, packingSlipItems);
+    }
+}
