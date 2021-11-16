21043b7579e712c904afc586cad6acbba9f278e1
ESipalis <eimantas98@gmail.com>
2021-11-16 04:52:20 +0100

    Application: implement `AddAgentCommissionCommandHandler`.

---
+++ Application/CommandHandlers/AddAgentCommissionCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services;
+using Domain.Commands;
+using MediatR;
+
+namespace Application.CommandHandlers;
+
+public class AddAgentCommissionCommandHandler : ICommandHandler<AddAgentCommissionCommand>
+{
+    private readonly IAgentCommissionService _agentCommissionService;
+
+
+    public AddAgentCommissionCommandHandler(IAgentCommissionService agentCommissionService)
+    {
+        _agentCommissionService = agentCommissionService;
+    }
+
+    public async Task<Unit> Handle(AddAgentCommissionCommand command, CancellationToken cancellationToken)
+    {
+        await _agentCommissionService.AddCommissionForAgent(command.AgentName, command.Amount);
+        return Unit.Value;
+    }
+}
